001e29f574233acddf5c0d469461c975bed11c75
sochypvp <thunsochy@gmail.com>
2025-08-10 11:01:42 +0700

    v001

---
+++ order/src/main/java/com/sochy/order/JuiiController.java
@@ -0,0 +1,16 @@
+package com.sochy.order;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("orders")
+public class JuiiController {
+    @GetMapping
+    public List<String> index(){
+        return List.of("juii","ot","ss");
+    }
+}
